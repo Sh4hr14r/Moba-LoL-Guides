49f4123c1dd5316a99d3301c7d7e40b16e81e0de
Alireza Sanaee <sarsanaee@gmail.com>
2013-03-12 22:20:34 +0330

    Functions related to database

--- make_database.py
+++ make_database.py
@@ -3,13 +3,12 @@
 	Add_to_database function to put thing or information in the database.
 '''
 import pymongo
-class database(object):
-	def __init__(self,Object):
-		self.post = Object
-		self.conn = pymongo.Connection()
-		self.db = self.conn['mobafire']
-		self.db['collect1']
-		
-	def Add_to_database(self,self.post.__dict__):
-		'''adding infomations to database'''
-		self.db.collect1.insert(self.post.__dict__)
+
+
+def make_database(dic):
+	''' this function try to make database'''
+	conn = pymongo.Connection()
+	db = conn["Mobafire"]
+	db["League of Legends"]
+	
+	
